b03a5be04ea1e1d7fdf8e9e8b3f3a3ab221c58c5
Detnex <manjeetit3026@gmail.com>
2019-08-10 18:01:56 +0530

    commit

--- src/videos.js
+++ src/videos.js
@@ -4,66 +4,66 @@
     id: 1,
     name: 'bonfire',
     username: 'Bonfire',
-    src: '/Videos/bonfire.mp4'
+    src: '../Videos/bonfire.mp4'
   },
   {
     id: 2,
     name: 'sky',
     username: 'Sky',
-    src: '/Videos/sky.mp4'
+    src: '../Videos/sky.mp4'
   },
   {
     id: 3,
     name: 'BUNNY',
     username: 'Big bug',
-    src: '/Videos/bunny.mp4'
+    src: '../Videos/bunny.mp4'
   },
   {
     id: 4,
     name: 'cactus',
     username: 'Cactus',
-    src: '/Videos/cactus.mp4'
+    src: '../Videos/cactus.mp4'
   },
   {
     id: 5,
     name: 'snow',
     username: 'Snow',
-    src: '/Videos/snow.mp4'
+    src: '../Videos/snow.mp4'
   },
   {
     id: 6,
     name: 'desert',
     username: 'Desert',
-    src: '/Videos/desert.mp4'
+    src: '../Videos/desert.mp4'
   },
   {
     id: 7,
     name: 'forest',
     username: 'Forest',
-    src: '/Videos/forest.mp4'
+    src: '../Videos/forest.mp4'
   },
   {
     id: 8,
     name: 'land',
     username: 'Land',
-    src: '/Videos/land.mp4'
+    src: '../Videos/land.mp4'
   },
   {
     id: 9,
     name: 'seaface',
     username: 'Seaface',
-    src: '/Videos/seaface.mp4'
+    src: '../Videos/seaface.mp4'
   },
   {
     id: 10,
     name: 'sky',
     username: 'Sky',
-    src: '/Videos/sky.mp4'
+    src: '../Videos/sky.mp4'
   },
   {
     id: 11,
     name: 'snow',
     username: 'Snow',
-    src: '/Videos/snow.mp4'
+    src: '../Videos/snow.mp4'
   }
 ];
