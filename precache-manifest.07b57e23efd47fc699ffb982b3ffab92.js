c6bcc4bfec80ba2c42f1030fae723c998e42ef58
Detnex <manjeetit3026@gmail.com>
2019-08-10 16:28:50 +0530

    Updates

---
+++ precache-manifest.07b57e23efd47fc699ffb982b3ffab92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8691d7dc802a2533239f05c7f7c8f165",
+    "url": "/newtube/index.html"
+  },
+  {
+    "revision": "6dbb349ce06513b31565",
+    "url": "/newtube/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "ff0562a9925a01a07ba8",
+    "url": "/newtube/static/css/main.b8cf6e00.chunk.css"
+  },
+  {
+    "revision": "6dbb349ce06513b31565",
+    "url": "/newtube/static/js/2.cfda6d96.chunk.js"
+  },
+  {
+    "revision": "ff0562a9925a01a07ba8",
+    "url": "/newtube/static/js/main.db707e2a.chunk.js"
+  },
+  {
+    "revision": "0ae255d6ed4ea5d4c880",
+    "url": "/newtube/static/js/runtime~main.abb3c4a7.js"
+  }
+]);
